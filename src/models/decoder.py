9ebf8c492e523051e3351bf9bb8a0845578d7f92
Junibuni <rich4103@naver.com>
2025-07-10 13:56:34 +0900

    Fusion in decoder, and add film layers

--- src/models/decoder.py
+++ src/models/decoder.py
@@ -3,9 +3,10 @@ import math
 import torch
 import torch.nn as nn
 from torch_geometric.data import Data
+from torch_geometric.nn import GraphNorm
 
 from src.models.data_utils import MAX_ATOMIC_NUM
-from src.models.embedder import LatentConditionEmbedder
+from src.models.embedder import FiLMLayer, LatentConditionEmbedder
 from src.models.unicrystalformer import CartNet_layer, MatformerConv
 
 class EdgeFeatureModulator(nn.Module):
@@ -30,7 +31,18 @@ class EdgeFeatureModulator(nn.Module):
             nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim)
         )
+        
+        self.reset_parameters()
     
+    def reset_parameters(self):
+        self.rbf_proj.reset_parameters()
+        self.rbf3_proj.reset_parameters()
+        self.cbf3_proj.reset_parameters()
+
+        for layer in self.fuse:
+            if hasattr(layer, "reset_parameters"):
+                layer.reset_parameters()
+            
     def angular_pooling(self, rbf_W1, sph):
         # rbf_W1: [N_edges, H, L]
         # sph:    [N_edges, K, L]
@@ -83,14 +95,16 @@ class CrystalDecoder(nn.Module):
         rbf_dim=16,
         cbf_dim=16,
         num_atom_types=MAX_ATOMIC_NUM,
-        num_message_layers=6,
+        num_message_layers=3,
         cutoff=5.0,
+        dropout=0.1,
     ):
         super().__init__()
         self.hidden_dim = hidden_dim
         self.latent_dim = latent_dim
         self.cutoff = cutoff
         self.edge_feature_dim = hidden_dim
+        self.num_layers = num_message_layers
 
         self.embedder = LatentConditionEmbedder(
             latent_dim=latent_dim,
@@ -111,18 +125,31 @@ class CrystalDecoder(nn.Module):
         
         self.cart_layers = nn.ModuleList([
             CartNet_layer(dim_in=hidden_dim, radius=self.cutoff) 
-            for _ in range(num_message_layers)
+            for _ in range(self.num_layers)
         ])
         self.mat_layers = nn.ModuleList([
             MatformerConv(in_channels=hidden_dim, out_channels=hidden_dim,
                           heads=4, edge_dim=self.edge_feature_dim,
                           concat=False, beta=True)
-            for _ in range(num_message_layers)
+            for _ in range(self.num_layers)
         ])
+        
+        self.norm_attn = nn.ModuleList([GraphNorm(hidden_dim) for _ in range(self.num_layers)])
+        self.ffn_layers = nn.ModuleList([nn.Sequential(
+                                    nn.Linear(hidden_dim, 2*hidden_dim),
+                                    nn.SiLU(),
+                                    nn.Linear(2*hidden_dim, hidden_dim)
+                                    ) for _ in range(self.num_layers)])
+        self.norm_ffn = nn.ModuleList([GraphNorm(hidden_dim) for _ in range(self.num_layers)])
 
         self.coord_head = nn.Linear(hidden_dim, 3)  # Predicts diff to coordinates
         self.type_head = nn.Linear(hidden_dim, num_atom_types)
-
+        
+        self.coord_head.reset_parameters()
+        self.type_head.reset_parameters()
+        
+        self.film_layer = FiLMLayer(hidden_dim)
+        self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, z, pred_frac_coords, pred_atom_types, num_atoms, lengths, angles, target_property):
         """
@@ -134,6 +161,8 @@ class CrystalDecoder(nn.Module):
             lengths: (N_cryst, 3)
             angles: (N_cryst, 3)
         """
+        condition = target_property.unsqueeze(1)  # (N_cryst, 1)
+        
         (
             h, # node features
             m, # edge features
@@ -148,21 +177,35 @@ class CrystalDecoder(nn.Module):
         edge_attr = self.edge_modulator(
             m, rbf_h, rbf3, cbf3, idx_s, idx_t
         )  # [N_edges, hidden_dim]
-        
-        data = Data(
-            x=h,                          # node features
-            edge_attr=edge_attr,          # edge features
-            edge_index=edge_index,        # graph
-            cart_dist=D_st,               # edge distances
-            batch=batch
-        )
             
         # Message passing
-        for layer in self.layers:
-            data = layer(data)
+        for i in range(self.num_layers):
+            local_data = Data(
+                x=h,                          # node features
+                edge_attr=edge_attr,          # edge features
+                edge_index=edge_index,        # graph
+                cart_dist=D_st,               # edge distances
+                batch=batch
+            )           
+            local_data = self.cart_layers[i](local_data)
+            h_local = local_data.x # [N, hidden_dim]
+            
+            h_global = self.mat_layers[i](h, edge_index, edge_attr) # [N, hidden_dim]
+            
+            h_comb = h_local + h_global
+            h_comb_norm = self.norm_attn[i](h_comb, batch)
+            h_attn = h + self.dropout(h_comb_norm)
+            
+            h_ffn = self.ffn_layers[i](h_attn)
+            h_ffn_norm = self.norm_ffn[i](h_ffn, batch)
+            h = h_attn + self.dropout(h_ffn_norm)
+            
+            h = self.film_layer(h, condition, num_atoms)
+
+
 
         # Outputs
-        pred_cart_coord_diff = self.coord_head(data.x)
-        pred_atom_type_logits = self.type_head(data.x)
+        pred_cart_coord_diff = self.coord_head(h)
+        pred_atom_type_logits = self.type_head(h)
 
         return pred_cart_coord_diff, pred_atom_type_logits

e24844a296f97886fc6f4858069650cff7ffa047
Junibuni <rich4103@naver.com>
2025-07-15 09:03:32 +0900

    Add Lanvegin dynamics

--- src/models/cgcvae.py
+++ src/models/cgcvae.py
@@ -8,6 +8,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from torch_scatter import scatter
+from tqdm import tqdm
 
 from src.models.data_utils import (MAX_ATOMIC_NUM, cart_to_frac_coords,
                                    frac_to_cart_coords, min_distance_sqr_pbc)
@@ -176,6 +177,74 @@ class ConditionalGraphVAE(_BaseModel):
         kld_loss = torch.mean(
             -0.5 * torch.sum(1 + log_var - mu**2 - log_var.exp(), dim=1), dim=0)
         return kld_loss
+    
+    @torch.no_grad()
+    def langevin_dynamics(self, z, ld_kwargs, gt_num_atoms=None, gt_atom_types=None):
+        if ld_kwargs.save_traj:
+            all_frac_coords = []
+            all_pred_cart_coord_diff = []
+            all_noise_cart = []
+            all_atom_types = []
+
+        # obtain key stats.
+        num_atoms, _, lengths, angles, composition_per_atom = self.decode_stats(
+            z, gt_num_atoms)
+        if gt_num_atoms is not None:
+            num_atoms = gt_num_atoms
+
+        # obtain atom types.
+        composition_per_atom = F.softmax(composition_per_atom, dim=-1)
+        if gt_atom_types is None:
+            cur_atom_types = self.sample_composition(
+                composition_per_atom, num_atoms)
+        else:
+            cur_atom_types = gt_atom_types
+
+        # init coords.
+        cur_frac_coords = torch.rand((num_atoms.sum(), 3), device=z.device)
+
+        # annealed langevin dynamics.
+        for sigma in tqdm(self.sigmas, total=self.sigmas.size(0), disable=ld_kwargs.disable_bar):
+            if sigma < ld_kwargs.min_sigma:
+                break
+            step_size = ld_kwargs.step_lr * (sigma / self.sigmas[-1]) ** 2
+
+            for step in range(ld_kwargs.n_step_each):
+                noise_cart = torch.randn_like(
+                    cur_frac_coords) * torch.sqrt(step_size * 2)
+                pred_cart_coord_diff, pred_atom_types = self.decoder(
+                    z, cur_frac_coords, cur_atom_types, num_atoms, lengths, angles)
+                cur_cart_coords = frac_to_cart_coords(
+                    cur_frac_coords, lengths, angles, num_atoms)
+                pred_cart_coord_diff = pred_cart_coord_diff / sigma
+                cur_cart_coords = cur_cart_coords + step_size * pred_cart_coord_diff + noise_cart
+                cur_frac_coords = cart_to_frac_coords(
+                    cur_cart_coords, lengths, angles, num_atoms)
+
+                if gt_atom_types is None:
+                    cur_atom_types = torch.argmax(pred_atom_types, dim=1) + 1
+
+                if ld_kwargs.save_traj:
+                    all_frac_coords.append(cur_frac_coords)
+                    all_pred_cart_coord_diff.append(
+                        step_size * pred_cart_coord_diff)
+                    all_noise_cart.append(noise_cart)
+                    all_atom_types.append(cur_atom_types)
+
+        output_dict = {'num_atoms': num_atoms, 'lengths': lengths, 'angles': angles,
+                       'frac_coords': cur_frac_coords, 'atom_types': cur_atom_types,
+                       'is_traj': False}
+
+        if ld_kwargs.save_traj:
+            output_dict.update(dict(
+                all_frac_coords=torch.stack(all_frac_coords, dim=0),
+                all_atom_types=torch.stack(all_atom_types, dim=0),
+                all_pred_cart_coord_diff=torch.stack(
+                    all_pred_cart_coord_diff, dim=0),
+                all_noise_cart=torch.stack(all_noise_cart, dim=0),
+                is_traj=True))
+
+        return output_dict
     
     def decode_stats(self, z, gt_num_atoms=None, gt_lengths=None, gt_angles=None, teacher_forcing=False):
         if gt_num_atoms is not None:
